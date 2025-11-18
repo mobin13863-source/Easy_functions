0f52f8ea3084ca0871f43699b13c45f53488d92c
Mobin <234991811+mobin13863-source@users.noreply.github.com>
2025-11-18 09:42:12 +0330

    map_zarb
    i use map in this code

--- anonymous.py
+++ anonymous.py
@@ -1,7 +1,7 @@
 def is_ashari(x):
     return not(x == int(x))
 
-a = [1, 2.3 , 34.3, 34, 667, 34.5]
+a = [1, 2.3 , 34.3, 34, 667, 34.5]  
 
 
 print(list(filter(lambda x: not(x == int(x)), a)))
---
+++ map_zarb.py
@@ -0,0 +1,5 @@
+a = [2, 3, 4, 5, 6,7 ,8 ]
+
+zarb_a = map(lambda x:  x * 2, a)
+
+print(list(zarb_a))
